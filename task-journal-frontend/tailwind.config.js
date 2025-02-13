5f2822ba4ed789809ea7c50ec8c686af54b9266b
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-02-13 19:14:38 +0100

    Added more work on login page

--- task-journal-frontend/tailwind.config.js
+++ task-journal-frontend/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// Colors haven't been able to be loaded
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#23C686",
+        "primary-light": "#2DEDA4",
+        "primary-dark": "#1CA670",
+        secondary: "#1D3853",
+        "secondary-light": "#7A9295",
+      },
+    },
   },
   plugins: [],
-}
-
+};
