d64761df64e4cee4b3660730b1afd140a4c6b5ab
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-03-04 19:15:20 +0100

    Added side tools on task page and changes on title handling on icon component

--- task-journal-frontend/src/app/components/icon/icon.component.ts
+++ task-journal-frontend/src/app/components/icon/icon.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { taskIcons } from '../select/select.constants';
 
 @Component({
   selector: 'app-icon',
@@ -11,4 +12,17 @@ import { Component, Input } from '@angular/core';
 export class IconComponent {
   @Input() iconName: string = '';
   @Input() iconSize: string = '';
+  @Input() titleString: string | null = null;
+
+  findTitle(): string | null {
+    if(this.titleString !== null) {
+      return this.titleString;
+    }
+    let iconObj = taskIcons.find((icon) => icon.id === this.iconName);
+    if(iconObj === undefined) {
+      return null;
+    } else {
+      return iconObj.label; 
+    }
+  }
 }
