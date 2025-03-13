0c3356167715c814e9b76accd10dea5d917fe755
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-03-13 12:14:07 +0100

    Work on tasks history page

--- task-journal-frontend/src/app/components/task-history-row/task-history-row.component.ts
+++ task-journal-frontend/src/app/components/task-history-row/task-history-row.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-history-row',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './task-history-row.component.scss'
 })
 export class TaskHistoryRowComponent {
-
+  @Input() taskHistory: any = {};
 }
--- task-journal-frontend/src/app/pages/tasks-history/tasks-history.component.ts
+++ task-journal-frontend/src/app/pages/tasks-history/tasks-history.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { taskIcons } from '../../components/select/select.constants';
+import { Router } from '@angular/router';
+import { BaseService } from '../../services/base/base.service';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-tasks-history',
   templateUrl: './tasks-history.component.html',
   styleUrl: './tasks-history.component.scss'
 })
-export class TasksHistoryComponent {
-  tasks: any = [];
+export class TasksHistoryComponent implements OnInit {
+  tasksHistory: any = [];
   filtersForm: FormGroup = new FormGroup({
     searchName: new FormControl(null), 
     searchIcons: new FormControl(null),
@@ -17,8 +20,29 @@ export class TasksHistoryComponent {
   icons = taskIcons;
   getTasksHistoryCallback: Function = () => {};
 
-  getTasksHistory(): void {
+  constructor(
+    private router: Router,
+    private baseService: BaseService
+  ) {}
 
+  ngOnInit(): void {
+    this.getTasksHistory();
+  }
+
+  getTasksHistory(): void {
+    let queryParameters: any = {};
+    Object.keys(this.filtersForm.controls).forEach((controlName) => {
+      queryParameters[controlName] = this.filtersForm.get(controlName)?.value;
+    });
+    this.baseService.get('tasks_history', queryParameters)
+    .pipe(
+      catchError((error) => {
+        return of(null);
+      })
+    )
+    .subscribe((data) => {
+      this.tasksHistory = data;
+    });
   }
 
   getNameControl() {
