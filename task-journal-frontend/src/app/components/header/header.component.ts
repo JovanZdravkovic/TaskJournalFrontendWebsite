5c14d88ddecc39e1e26e805b110f4101817d4e70
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-03-18 14:34:40 +0100

    Updated header active route recognition

--- task-journal-frontend/src/app/components/header/header.component.ts
+++ task-journal-frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent{
+  constructor(private router: Router) {}
 
+  activeRoutes(routes: string[]): boolean {
+    let truncatedRoute: string = this.router.url;
+    let firstSignificantSlash: number = truncatedRoute.indexOf('/', 1);
+    if(firstSignificantSlash !== -1) {
+      truncatedRoute = truncatedRoute.substring(0, firstSignificantSlash);
+    }
+    return routes.some((route) => route === truncatedRoute);
+  }
 }
