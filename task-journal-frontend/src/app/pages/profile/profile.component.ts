7a4fc89558f228d9280cfc8a51ef75a58b77730f
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2025-03-20 12:46:36 +0100

    Added work on profile page

--- task-journal-frontend/src/app/pages/profile/profile.component.ts
+++ task-journal-frontend/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  profile: any = {};
+  editMode: boolean = false;
+  editForm: FormGroup | null = null;
+  selectedProfileIconName: string = '';
 
+  initEditForm(): void {
+      this.editForm = new FormGroup({
+        username: new FormControl(this.profile.username, Validators.required),
+        email: new FormControl(this.profile.email, Validators.required),
+      });
+    }
+
+  editProfile(): void {
+    this.editMode = !this.editMode;
+    this.initEditForm();
+  }
+
+  updateProfile(): void {
+    
+  }
+
+  selectedProfileIcon($event: any): void {
+    this.selectedProfileIconName = $event.target.files[0].name;
+  }
 }
